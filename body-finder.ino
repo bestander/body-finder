209d1a1e3b8d36868fa2bc2a8a069c3aa44f8d25
Konstantin Raev <bestander@gmail.com>
2021-01-30 19:01:46 -0800

    gps + radio initial test

--- body-finder.ino
+++ body-finder.ino
@@ -4,11 +4,39 @@
 #include <Wire.h>
 #include "cubecell_SSD1306Wire.h"
 
+#define RF_FREQUENCY 868000000 // Hz
+
+#define TX_OUTPUT_POWER 20 // dBm
+
+#define LORA_BANDWIDTH 0        // [0: 125 kHz, \
+                                //  1: 250 kHz, \
+                                //  2: 500 kHz, \
+                                //  3: Reserved]
+#define LORA_SPREADING_FACTOR 8 // [SF7..SF12]
+#define LORA_CODINGRATE 4       // [1: 4/5, \
+                                //  2: 4/6, \
+                                //  3: 4/7, \
+                                //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH 8  // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT 0   // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON false
+#define LORA_IQ_INVERSION_ON false
+
+#define RX_TIMEOUT_VALUE 1000
+#define BUFFER_SIZE 30 // Define the payload size here
+
+char txpacket[BUFFER_SIZE];
+char rxpacket[BUFFER_SIZE];
+
+static RadioEvents_t RadioEvents;
+void OnTxDone(void);
+void OnTxTimeout(void);
+
 SSD1306Wire display(0x3c, 500000, I2C_NUM_0, GEOMETRY_128_64, GPIO10); // addr , freq , i2c group , resolution , rst
 
 int fracPart(double val, int n)
 {
-  return (int)((val - (int)(val)) * pow(10, n));
+  return abs((int)((val - (int)(val)) * pow(10, n)));
 }
 
 void VextON(void)
@@ -28,14 +56,30 @@ void setup()
   VextON();
   delay(10);
 
+  boardInitMcu();
   display.init();
   display.clear();
   display.display();
 
   display.setTextAlignment(TEXT_ALIGN_CENTER);
   display.setFont(ArialMT_Plain_16);
-  display.drawString(64, 32 - 16 / 2, "GPS initing...");
+  display.drawString(64, 32 - 16 / 2, "GPS starting...");
   display.display();
+
+  RadioEvents.TxDone = OnTxDone;
+  RadioEvents.TxTimeout = OnTxTimeout;
+  RadioEvents.RxDone = OnRxDone;
+
+  Radio.Init(&RadioEvents);
+  Radio.SetChannel(RF_FREQUENCY);
+  Radio.SetTxConfig(MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                    LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                    LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                    true, 0, 0, LORA_IQ_INVERSION_ON, 3000);
+  Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                    LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                    LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                    0, true, 0, 0, LORA_IQ_INVERSION_ON, true);
 
   Serial.begin(115200);
   Air530.begin();
@@ -55,44 +99,56 @@ void loop()
   char str[30];
   display.clear();
   display.setFont(ArialMT_Plain_10);
-  int index = sprintf(str, "%02d-%02d-%02d", Air530.date.year(), Air530.date.day(), Air530.date.month());
-  str[index] = 0;
   display.setTextAlignment(TEXT_ALIGN_LEFT);
+
+  int index = sprintf(str, "Me: sats: %d", (int)Air530.satellites.value());
+  str[index] = 0;
   display.drawString(0, 0, str);
 
-  index = sprintf(str, "%02d:%02d:%02d", Air530.time.hour(), Air530.time.minute(), Air530.time.second(), Air530.time.centisecond());
+  index = sprintf(str, "%02d:%02d:%02d", Air530.time.hour(), Air530.time.minute(), Air530.time.second());
   str[index] = 0;
   display.drawString(60, 0, str);
 
-  if (Air530.location.age() < 1000)
-  {
-    display.drawString(120, 0, "A");
-  }
-  else
-  {
-    display.drawString(120, 0, "V");
-  }
-
-  index = sprintf(str, "alt: %d.%d", (int)Air530.altitude.meters(), fracPart(Air530.altitude.meters(), 2));
+  index = sprintf(str, "lat:%d.%d lon:%d.%d",
+                  (int)Air530.location.lat(),
+                  fracPart(Air530.location.lat(), 4),
+                  (int)Air530.location.lng(),
+                  fracPart(Air530.location.lng(), 4));
   str[index] = 0;
   display.drawString(0, 16, str);
 
-  index = sprintf(str, "hdop: %d.%d", (int)Air530.hdop.hdop(), fracPart(Air530.hdop.hdop(), 2));
+  index = sprintf(str, "Buddy: distance: %dm", 0);
   str[index] = 0;
   display.drawString(0, 32, str);
 
-  index = sprintf(str, "lat :  %d.%d", (int)Air530.location.lat(), fracPart(Air530.location.lat(), 4));
-  str[index] = 0;
-  display.drawString(60, 16, str);
-
-  index = sprintf(str, "lon: %d.%d", (int)Air530.location.lng(), fracPart(Air530.location.lng(), 4));
-  str[index] = 0;
-  display.drawString(60, 32, str);
-
-  Serial.print(Air530.satellites.value());
-
-  index = sprintf(str, "satelites: %d", (int)Air530.satellites.value());
-  str[index] = 0;
+  index = sprintf(str, "lat:%d.%d lon:%d.%d",
+                  (int)Air530.location.lat(),
+                  fracPart(Air530.location.lat(), 4),
+                  (int)Air530.location.lng(),
+                  fracPart(Air530.location.lng(), 4));
   display.drawString(0, 48, str);
+
   display.display();
+}
+
+void OnTxDone(void)
+{
+  Serial.print("TX done!");
+  // turnOnRGB(0, 0);
+}
+
+void OnTxTimeout(void)
+{
+  Radio.Sleep();
+  Serial.print("TX Timeout......");
+  // state=ReadVoltage;
+}
+
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr)
+{
+  rssi = rssi;
+  memcpy(rxpacket, payload, size);
+  rxpacket[size] = '\0';
+  Radio.Sleep();
+  Serial.printf("\r\nreceived packet \"%s\" with rssi %d , length %d\r\n", rxpacket, rssi, size);
 }
